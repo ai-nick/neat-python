25de7af3530da50a4583d9473ea807f873bd66b3
Nicholas Williams <nick5664@outlook.com>
2018-10-15 19:23:55 +0000

    reduced tanh and sigmoid to barebones, was getting wierd results with the first lines uncommented

--- neat/activations.py
+++ neat/activations.py
@@ -6,14 +6,16 @@ and code for adding new user-defined ones
 from __future__ import division
 import math
 import types
-import numpy as np
+#import numpy as np
+
 
 def sigmoid_activation(z):
-    return 1.0 / (1.0 + np.exp(-z))
+    #z = max(-60.0, min(60.0, 5.0 * z))
+    return 1.0 / (1.0 + math.exp(-z))
 
 
 def tanh_activation(z):
-    z = max(-60.0, min(60.0, 2.5 * z))
+    #z = max(-60.0, min(60.0, 2.5 * z))
     return math.tanh(z)
 
 
